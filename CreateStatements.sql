b86d9b7ee11dba1533c7d48821befeb348435c0c
Drake Lambert <drake.askme2@gmail.com>
2018-10-23 13:25:12 -0500

    added create statements

--- CreateStatements.sql
+++ CreateStatements.sql
@@ -1,19 +1,52 @@
-create table Users(
+create table ApplicationUser(
   Id char(16),
   Username varchar(100) not null,
   Passwordhash varchar(100) not null,
   primary key ( Id )
 );
-create table Boards(
+create table Board(
   Id char(16),
   Title varchar(100) not null,
   OwnerId char(16),
   primary key ( id ),
-  foreign key ( OwnerId ) references Users( Id )
+  foreign key ( OwnerId ) references ApplicationUser( Id )
 );
-create table BoardMembers(
+create table BoardMember(
   BoardId char(16),
   MemberId char(16),
   foreign key ( BoardId ) references Board( Id ),
-  foreign key ( MemberId ) references Users( Id )
+  foreign key ( MemberId ) references ApplicationUser( Id )
 );
+create table List(
+  Id char(16),
+  Title varchar(100) not null,
+  BoardPosition int not null,
+  BoardId char(16),
+  primary key ( id ),
+  foreign key ( BoardId ) references Board( Id )
+);
+create table Task(
+  Id char(16),
+  Title varchar(100) not null,
+  Descriptor varchar(500) not null,
+  DueDate datetimeoffset,
+  ListId char(16),
+  primary key ( id ),
+  foreign key ( ListId ) references List( Id )
+)
+create table TaskAssignee(
+  TaskId char(16),
+  AssigneeId char(16),
+  foreign key ( TaskId ) references Task( Id ),
+  foreign key ( AssigneeId ) references ApplicationUser( Id )
+)
+create table Comment(
+  Id char(16),
+  TimeCreated datetimeoffset not null,
+  MessageText varchar(500) not null,
+  TaskId char(16),
+  OwnerId char(16),
+  primary key ( id ),
+  foreign key ( TaskId ) references Task( Id ),
+  foreign key ( OwnerId ) references ApplicationUser( Id )
+)
